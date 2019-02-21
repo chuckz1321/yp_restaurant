cdddf33ba5a3ae955954ce32049bf33ab6dc8874
zcy960312 <zcy960312@gmail.com>
2019-02-20 22:17:56 -0500

    solve swagger error

--- src/main/java/com/example/yp_restaurant/Swagger/SwaggerConfig.java
+++ src/main/java/com/example/yp_restaurant/Swagger/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.example.yp_restaurant.Swagger;
 
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
     public Docket createRestApi(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
