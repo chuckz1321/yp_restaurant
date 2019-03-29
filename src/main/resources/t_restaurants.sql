5f61271d527fca1c66f4db3214d5a4dc333124c4
Wang LYU <wlyu2@uwo.ca>
2019-03-28 22:16:18 -0400

    remove the redundant column

--- src/main/resources/t_restaurants.sql
+++ src/main/resources/t_restaurants.sql
@@ -1,8 +1,8 @@
 create table t_restaurants(
-    NAME_OF_INSTITUTION varchar(100) not null,
     ADDRESS varchar(100) not null,
     CATEGORIES varchar(100) not null,
     CITY varchar(100) not null,
+    COUNTRY varchar(100) not null,
     NAME varchar(100) not null,
     POSTALCODE varchar(100) not null,
     PROVINCE varchar(2) not null
